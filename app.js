7fc2dab6370300b5c8b612e64cf733504b2086fc
Teerapat Aksonsarasit <plovet2032@gmail.com>
2022-03-08 02:04:34 +0700

    build: node create start project

---
+++ app.js
@@ -0,0 +1,46 @@
+// Config
+
+const express = require("express");
+const path = require("path");
+const cookieSession = require("cookie-session");
+const fetch = require("node-fetch");
+const dotenv = require("dotenv");
+
+// dotenv.config();
+dotenv.config({ path: "./.env" });
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+
+app.use(express.static('views'));
+app.use(express.static('public'));
+
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/script", express.static(__dirname + "public/script"));
+app.use("/layouts", express.static(__dirname + "views/layouts"));
+app.use("/partials", express.static(__dirname + "views/partials"));
+
+app.use(
+    cookieSession({
+        name: "session",
+        keys: [ process.env.cookieKey ],
+        maxAge: 60 * 1000,
+    }),
+);
+
+// Route
+
+app.get("/", (req, res, next) => {
+    res.render("layouts/home", {
+        title: "Read Like a Book | Home",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log("Running on PORT:" + PORT);
+});
